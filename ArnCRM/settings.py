dfe3dbfcff704ecfb2eb8c2f0aa87f0c57b3d246
Aekaka <rinyoe@gmail.com>
2023-11-10 18:53:39 +0300

    registration

--- ArnCRM/settings.py
+++ ArnCRM/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'website',
+
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'ArnCRM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'ArnoldCRM',
+        'USER':'root',
+        'PASSWORD':'664209.Ab',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
--- ArnCRM/urls.py
+++ ArnCRM/urls.py
@@ -7,7 +7,11 @@ Examples:
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
+Class-based views  
+
+path('admin/', admin.site.urls),
+
+
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
@@ -15,8 +19,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('website.urls')),
 ]
